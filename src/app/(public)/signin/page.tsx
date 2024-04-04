a9ab8aac7805c23141e132cd06c4d54eb51b617d
samhou1988 <samhou1988@gmail.com>
2024-04-04 20:31:10 +0800

    feat: login and signup

---
+++ src/app/(public)/signin/page.tsx
@@ -0,0 +1,9 @@
+import Form from "./form";
+
+export default async function SignIn() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <Form />
+    </div>
+  );
+}
---
+++ src/app/(public)/signup/page.tsx
@@ -0,0 +1,9 @@
+import Form from "./form";
+
+export default async function SignUp() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Form />
+    </div>
+  );
+}
