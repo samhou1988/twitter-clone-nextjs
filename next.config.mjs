a5d0044c599080553f080564e1fc67c3b73bdd78
samhou1988 <samhou1988@gmail.com>
2024-04-04 21:02:22 +0800

    feat: account

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xgr3seh2mgmhmtft.public.blob.vercel-storage.com'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
---
+++ src/app/(private)/account/avatar-form.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import useSWR from "swr";
+
+import { fetcher } from "@/utils/fetcher";
+
+export default function AvatarForm() {
+  const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const user = data.data;
+
+  return (
+    <div>
+      {user.avatar && (
+        <div>
+          <Image
+            src={user.avatar}
+            alt={user.avatar}
+            width={200}
+            height={200}
+            className="rounded-full m-auto my-5"
+          />
+        </div>
+      )}
+      {!user.avatar && (
+        <div
+          className="bg-slate-600 rounded-full m-auto my-5"
+          style={{ width: 200, height: 200 }}
+        ></div>
+      )}
+      <Link
+        href="/avatar/upload"
+        className="dark:text-green-400 text-green-800 underline p-2 rounded-lg my-5"
+      >
+        Update Avatar
+      </Link>
+    </div>
+  );
+}
